b78d6311a8566e927bb0c2e79575f1cc65dcf765
SIMON Théo <theo.simon@etu.univ-lehavre.fr>
2026-06-01 17:06:27 +0200

    modif Liaison

--- source/metier/Liaison.java
+++ source/metier/Liaison.java
@@ -2,5 +2,33 @@ package source.metier;
 
 public class Liaison
 {
-
+	private Jeton planeteSource;
+	private Jeton planeteDestination;
+	
+	private String espece;
+	
+	public Liaison(Jeton planeteSource, Jeton planeteDestination, String espece)
+	{
+		this.planeteSource      = planeteSource;
+		this.planeteDestination = planeteDestination;
+		this.espece             = espece;
+	}
+	
+	public Jeton getPlaneteSource(){return this.planeteSource;}
+	public Jeton getPlaneteDestination(){return this.planeteDestination;}
+	public String getEspece(){return this.espece;}
+	
+	public boolean setEspece(String espece)
+	{
+		if(espece == null){return false;}
+		this.espece = espece;
+		return true;
+	}
+	
+	public String toString()
+	{
+		return "Planete source : "      + this.planetSource       +
+		       "Planete destination : " + this.planeteDestination +
+		       "Espece :              " + this.espece;
+	}
 }
