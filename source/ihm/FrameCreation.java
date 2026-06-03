84251595d1b4469bccc8f4aa655324135246eebb
SIMON Théo <theo.simon@etu.univ-lehavre.fr>
2026-06-03 16:34:30 +0200

    Changement nom class et modif mineure dans class

---
+++ source/ihm/FrameCreation.java
@@ -0,0 +1,45 @@
+package source.ihm;
+
+import source.Controleur;
+
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
+
+public class FrameCreation extends JFrame
+{
+	protected static final Font  POLICE_TEXTE  = new Font    ("Goldman", Font.BOLD, 25);
+	protected static final Color COULEUR_TITRE = Color.decode("#f1c232");
+	protected static final Color COULEUR_ZONE  = Color.decode("#f3f3f3");
+	protected static final Color COULEUR_FOND  = new Color (37, 37, 37);
+
+	private Controleur ctrl;
+	private PanelCreation  panelCreation;
+
+	public FrameAppli(Controleur ctrl) 
+	{
+		this.setTitle("Tourisme à Vitesse Grand C");
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+		/* ---------------------------------- */
+		/*       Création des composants      */
+		/* ---------------------------------- */
+
+		this.ctrl      = ctrl;
+		this.panelCreation = new PanelCreation(ctrl);
+
+		/* ---------------------------------- */
+		/*    Positionnement des composants   */
+		/* ---------------------------------- */
+
+		this.add(this.panelCreation);
+
+		/* ---------------------------------- */
+		/*      Activation des composants     */
+		/* ---------------------------------- */
+
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+
+	}
+}
