51a88fc8698e2f02a935e5c2e8c628dc23b6a4f7
Cochaux Kyerann <kyerann.cochaux@etu.univ-lehavre.fr>
2026-06-04 16:31:50 +0200

    TAB_FORMES -+ TAB_PLanete

--- source/metier/Metier.java
+++ source/metier/Metier.java
@@ -100,7 +100,7 @@ public class Metier
 					{
 						int indPlanete = Integer.parseInt("" + sIndPlanete);
 						
-						Planete tempPlanete = Planete.creerPlanete(Plateau.TAB_FORMES[indPlanete].charAt(0), null);
+						Planete tempPlanete = Planete.creerPlanete(Plateau.TAB_PLANETES[indPlanete].charAt(0), null);
 						
 						this.plateauJeu.getCase(numLig, numCol).setPlanete(tempPlanete);
 					}
