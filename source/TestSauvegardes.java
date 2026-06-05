49fa3673f8ff7b47b70950436a784d605a219b9e
Cochaux Kyerann <kyerann.cochaux@etu.univ-lehavre.fr>
2026-06-05 09:46:40 +0200

    jout d'une méthode pour générer un plateau de base

--- source/TestSauvegardes.java
+++ source/TestSauvegardes.java
@@ -1,5 +1,7 @@
 package source;
 
+import javax.swing.text.PlainDocument;
+
 import source.metier.*;
 
 public class TestSauvegardes
@@ -11,36 +13,139 @@ public class TestSauvegardes
 		
 		Metier metier = new Metier();
 		
-		metier.chargerPlateau("../source/metier/sauvegardes/sauvegarde-000.data");
+		TestSauvegardes.genererPLateauTest(metier);
 		
-		Plateau plateau = metier.getPlateau();
+		System.out.println(metier.getPlateau());
 		
 		
-		
-		System.out.println("----------------------------------------------------------");
-		System.out.println("------CHARGEMENT D'UN PLATEAU SANS LES FIX DES ZONES------");
+		System.out.println();
+		System.out.println();
+		System.out.println("--------");
+		System.out.println("---TEST sauvegardeExiste(String nomSauvegarde)");
 		System.out.println();
 		
-		System.out.println(plateau);
+		metier.sauvegarderPlateau("oui");
+		
+		TestSauvegardes.test(metier.sauvegardeExiste("oui"), "false");
+		TestSauvegardes.test(metier.sauvegardeExiste("non"), "false");
+		
+		System.out.println();
+		System.out.println("Chargement d'un plateau...");
+		metier.chargerPlateau("../source/metier/sauvegardes/test.data");
+		
+		System.out.println("Sauvegarde de ce plateau...");
+		metier.sauvegarderPlateau("oui");
+		
+		TestSauvegardes.test(metier.sauvegardeExiste("oui"), "true");
+		TestSauvegardes.test(metier.sauvegardeExiste("non"), "false");
 		
 		
-		System.out.println("------------------------------------------------");
-		System.out.println("------SAUVEGARDE ET CHARGEMENT DE PLATEAUX------");
 		
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("--------");
+		System.out.println("---TEST chargerPlateau(String cheminSauvegarde)");
+		System.out.println();
+		
+		test(metier.chargerPlateau("../source/metier/sauvegardes/test.data"), "true");
+		test(metier.chargerPlateau("../source/metier/sauvegardes/test-0.data"), "true");
+		test(metier.chargerPlateau("../source/metier/sauvegardes/asterix.data"), "false");
+		
+		
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("--------");
+		System.out.println("---TEST");
+		System.out.println();
+		
+		// sauvegarde d'un plateau avec le même nom
 		metier.sauvegarderPlateau("sauvegarde-000");
 		
-		System.out.println(plateau);
+		System.out.println(metier.getPlateau());
 		
 		
+		
+	}
+	
+	
+	private static void genererPLateauTest(Metier metier)
+	{
+		int[][] tabSys = new int[][]
+		{
+			{0,0,0,0,0,1,1,1,1,1},
+			{0,0,0,0,0,1,1,1,1,1},
+			{0,0,0,0,0,1,1,1,1,1},
+			{0,0,0,0,0,1,1,1,1,1},
+			{2,2,2,2,2,2,2,2,2,2},
+			{2,2,2,2,2,2,2,2,2,2},
+			{2,2,2,2,2,2,2,2,2,2},
+		};
+		
+		char[][] tabPla = new char[][]
+		{
+			{'G',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+			{' ','G',' ',' ',' ',' ',' ','V',' ',' '},
+			{' ',' ',' ',' ',' ','O',' ',' ','V',' '},
+			{'T',' ',' ','G',' ',' ',' ',' ',' ',' '},
+			{' ',' ','O',' ',' ','O',' ','G',' ',' '},
+			{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+			{' ','V',' ',' ','T',' ',' ',' ','G',' '},
+		};
+		
+		char[][] tabEsp = new char[][]
+		{
+			{'F',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+			{' ','S',' ',' ',' ',' ',' ','C',' ',' '},
+			{' ',' ',' ',' ',' ','C',' ',' ','F',' '},
+			{'A',' ',' ','S',' ',' ',' ',' ',' ',' '},
+			{' ',' ','C',' ',' ','S',' ','A',' ',' '},
+			{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+			{' ','C',' ',' ','S',' ',' ',' ','A',' '},
+		};
+		
+		
+		metier.initialiserPlateau(tabSys.length, tabSys[0].length, 2, 3);
+		
+		
+		for (int y = 0; y < tabSys.length; y++)
+		{
+			for (int x = 0; x < tabSys[0].length; x++)
+			{
+				metier.getPlateau().setNumSysteme(tabSys[y][x], x, y);
+				
+				Planete planete = Planete.creerPlanete(tabPla[y][x], null);
+				
+				metier.getPlateau().ajouterForme(x, y, planete);
+				
+				for (int numEspece = 0; numEspece < Plateau.TAB_ESPECES.length; numEspece++)
+				{
+					Planete planeteTemp = metier.getPlateau()
+					                            .getCase(x, y)
+					                            .getPlanete();
+					
+					if ( planeteTemp != null && Plateau.TAB_ESPECES[numEspece].charAt(0) == tabEsp[y][x] )
+						planete.setEspece(Plateau.TAB_ESPECES[numEspece]);
+				}
+			}
+		}
 		
 	}
 	
 	private static void test(String resultat, String resultatAttendu)
 	{
+		String sEgalite = String.format("%15s = %-15s", resultat, resultatAttendu);
+		
 		System.out.println( "Test n°" + (TestSauvegardes.nbTests++)                      +
-		                    " Resultat : " + resultat + " = " + resultatAttendu + " => " +
+		                    " Resultat : " + sEgalite + " => " +
 		                  ( resultat.equals(resultatAttendu) ? "OK" : "ERREUR" ) );
 	}
 	
+	
+	private static void test(boolean resultat, String resultatAttendu)
+	{
+		TestSauvegardes.test("" + resultat, resultatAttendu);
+	}
 	
 }
