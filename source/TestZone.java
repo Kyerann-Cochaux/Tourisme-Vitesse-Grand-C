f4c79b19d1a25a1ca2ab7a0cf2d8324f84d577b0
Cochaux Kyerann <kyerann.cochaux@etu.univ-lehavre.fr>
2026-06-05 14:25:47 +0200

    modif dans les test des zones

--- source/TestZone.java
+++ source/TestZone.java
@@ -14,7 +14,7 @@ public class TestZone
 		Metier m = new Metier();
 		Plateau p = null;
 		
-		m.initialiserPlateau(11,11,3,2);
+		m.initialiserPlateau(7,10,3,2);
 		
 		
 		p = m.getPlateau();
@@ -23,70 +23,116 @@ public class TestZone
 		System.out.println("/*           Ajout des Zones          */");
 		System.out.println("/* ---------------------------------- */");
 		
+		
+		
+		
 		// zone 0
 		
-		System.out.println("--------");
-		System.out.println(p.setNumSysteme(0, 0, 1 )                              == true  ? "OK" : "ERREUR"  );
-		System.out.println(p.setNumSysteme(0, 0, 0 )                              == true  ? "OK" : "ERREUR"  );
-		System.out.println(p.setNumSysteme(0, 1, 0 )                              == true  ? "OK" : "ERREUR"  );
-		System.out.println("Zone 0 Non adjacent : " + ( p.setNumSysteme(0, 2, 2 ) == false ? "OK" : "ERREUR") );
+		TestZone.test(p.setNumSysteme(0, 0, 0 ),  "true" );
+		TestZone.test(p.setNumSysteme(0, 1, 0 ),  "true" );
+		
+		// zone 0 non adjascente
+		TestZone.test(p.setNumSysteme(0, 2, 2 ),  "false");
+		
+		
+		
 		
 		
 		// zone 1
 		
-		System.out.println(p.setNumSysteme(1, 3, 0 )                             == true  ? "OK" : "ERREUR"   );
-		System.out.println("Zone 1 Non adjacent : " + (p.setNumSysteme(1, 5, 0 ) == false ? "OK" : "ERREUR" ) );
-		System.out.println(p.setNumSysteme(1, 3, 1 )                             == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(1, 4, 1 )                             == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(1, 5, 1 )                             == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(1, 5, 0 )                             == true  ? "OK" : "ERREUR"   );
-		System.out.println("y négatif : "          + ( p.setNumSysteme(1, 5, -1  ) == false ? "OK" : "ERREUR"  ) );
+		TestZone.test(p.setNumSysteme(1, 3, 0 )   , "true" );
 		
+		//Zone 1 Non adjacent : 
+		TestZone.test(p.setNumSysteme(1, 5, 0 ), "false" );
+		
+		TestZone.test(p.setNumSysteme(1, 3, 1 )   , "true" );
+		TestZone.test(p.setNumSysteme(1, 4, 1 )   , "true" );
+		TestZone.test(p.setNumSysteme(1, 5, 1 )   , "true" );
+		TestZone.test(p.setNumSysteme(1, 5, 0 )   , "true" );
+		
+		// y negatif
+		TestZone.test(p.setNumSysteme(1, 5, -1  ), "false" );
+		
+		
+		
+		
+		TestZone.test(p.setNumSysteme(0, 0, 1 ),  "true" );
 		
 		// zone 2
 		
-		System.out.println(p.setNumSysteme(2, 0, 4 )                            == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(2, 0, 3 )                            == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(2, 1, 3 )                            == true  ? "OK" : "ERREUR"   );
-		System.out.println("zone 1 isolé : "+ (p.setNumSysteme(1, 1, 4 )        == false ? "OK" : "ERREUR"  ) );
-		System.out.println("x négatif    : "+ (p.setNumSysteme(2, -1, 4 )         == false ? "OK" : "ERREUR"  ) );
+		TestZone.test(p.setNumSysteme(2, 0, 4 )   , "true" );
+		TestZone.test(p.setNumSysteme(2, 0, 3 )   , "true" );
+		TestZone.test(p.setNumSysteme(2, 1, 3 )   , "true" );
+		
+		
+		
+		// zone 1 isolée
+		TestZone.test(p.setNumSysteme(1, 1, 4 ),   "false" );
+		
+		// x negatif
+		TestZone.test(p.setNumSysteme(2, -1, 4 ) , "false" );
+		
+		
+		
+		
 		
 		// zone 3
+		TestZone.test(p.setNumSysteme(3, 0, 6 )   , "true" );
+		TestZone.test(p.setNumSysteme(3, 1, 6 )   , "true" );
+		TestZone.test(p.setNumSysteme(3, 0, 5 )   , "true" );
+		TestZone.test(p.setNumSysteme(3, 0, 5 )   , "true" );
 		
-		System.out.println(p.setNumSysteme(3, 0, 6 )                            == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(3, 1, 6 )                            == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(3, 0, 5 )                            == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(3, 0, 5 )                            == true  ? "OK" : "ERREUR"   );
+		
+		
+		
 		
 		// zone 4
-		System.out.println(p.setNumSysteme(4, 9, 5 )                            == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(4, 9, 6 )                            == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(4, 8, 6 )                            == true  ? "OK" : "ERREUR"   );
+		TestZone.test(p.setNumSysteme(4, 9, 5 )   , "true" );
+		TestZone.test(p.setNumSysteme(4, 9, 6 )   , "true" );
+		TestZone.test(p.setNumSysteme(4, 8, 6 )   , "true" );
+		
+		
+		
+		
 		
 		// zone 5
+		TestZone.test(p.setNumSysteme(5, 9, 3 )   , "true" );
+		TestZone.test(p.setNumSysteme(5, 8, 3 )   , "true" );
 		
-		System.out.println(p.setNumSysteme(5, 9, 3 )                                 == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(5, 8, 3 )                                 == true  ? "OK" : "ERREUR"   );
-		System.out.println("x trop grand : "           + (p.setNumSysteme(5, 10, 3 ) == false ? "OK" : "ERREUR" ) );
-		System.out.println("zone 1 déjà prise (top) : "+ (p.setNumSysteme(1, 8 , 2 ) == false ? "OK" : "ERREUR" ) );
-		System.out.println("zone 1 déjà prise (left): "+ (p.setNumSysteme(1, 7 , 3 ) == false ? "OK" : "ERREUR" ) );
-		System.out.println("zone 1 déjà prise (down): "+ (p.setNumSysteme(1, 7 , 4 ) == false ? "OK" : "ERREUR" ) );
+		//x trop grand
+		TestZone.test(p.setNumSysteme(5, 500, 3 ), "false" );
+		
+		// zone 1 deja prise
+		TestZone.test(p.setNumSysteme(1, 8  , 2 ), "false" );
+		
+		// zone 1 deja prise a gauche
+		TestZone.test(p.setNumSysteme(1, 7  , 3 ), "false" );
+		
+		// zone 1 deja prise en bas
+		TestZone.test(p.setNumSysteme(1, 7  , 4 ), "false" );
+		
+		
+		
+		
 		
 		// zone 6
+		TestZone.test(p.setNumSysteme(6,  9, 0 )   , "true" );
+		TestZone.test(p.setNumSysteme(6,  8, 0 )   , "true" );
+		TestZone.test(p.setNumSysteme(6,  7, 0 )   , "true" );
+		TestZone.test(p.setNumSysteme(6,  6, 0 )   , "true" );
+		TestZone.test(p.setNumSysteme(6,  5, 0 )   , "true" );
+		TestZone.test(p.setNumSysteme(6,  7, 1 )   , "true" );
 		
-		System.out.println(p.setNumSysteme(6, 9, 1 )                                 == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(6, 9, 0 )                                 == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(6, 8, 0 )                                 == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(6, 7, 0 )                                 == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(6, 6, 0 )                                 == true  ? "OK" : "ERREUR"   );
-		System.out.println(p.setNumSysteme(6, 5, 0 )                                 == true  ? "OK" : "ERREUR"   );
+		
+		
+		
 		
 		// zone 7
-		
-		System.out.println(p.setNumSysteme(7, 3, 6 )                                 == true  ? "OK" : "ERREUR"     );
-		System.out.println("y trop grand : " + (p.setNumSysteme(7, 3, 7 )            == false ? "OK" : "ERREUR"   ) );
+		TestZone.test(p.setNumSysteme(7, 3, 6 ), "true" );
+		// y trop grand
+		TestZone.test(p.setNumSysteme(7, 3, 700 ), "false");
 		System.out.println();
-		System.out.println(p.afficherSystemes() );
+		System.out.println(p);
 		
 		
 	}
